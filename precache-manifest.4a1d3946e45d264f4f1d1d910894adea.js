240ddfa9220217bc36e61844ef56458080f3457b
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-09 20:22:22 -0600

    Updates

---
+++ precache-manifest.4a1d3946e45d264f4f1d1d910894adea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa07437bb095b02b3b281042470835a",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "8bec04846e508a9da165",
+    "url": "/simple-calculator/static/css/main.3e0ad63b.chunk.css"
+  },
+  {
+    "revision": "cd06066ddc45853d1061",
+    "url": "/simple-calculator/static/js/2.579d7a58.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/simple-calculator/static/js/2.579d7a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bec04846e508a9da165",
+    "url": "/simple-calculator/static/js/main.0fa5b7d6.chunk.js"
+  },
+  {
+    "revision": "83cc37eff5fa3a261bb3",
+    "url": "/simple-calculator/static/js/runtime-main.61b1c7cb.js"
+  }
+]);
