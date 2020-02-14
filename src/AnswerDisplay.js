112903188a398bf4826b10f66867210a33fbc863
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-02-13 18:40:50 -0600

    Added blinking zero

--- src/AnswerDisplay.js
+++ src/AnswerDisplay.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 function AnswerDisplay(props){
+
   return (
-    <div className="display">
+    <div className={props.equation === "0" ? "display blink" : "display"}>
       <h1 className={props.equation.length < 10 ? "equation-big" : "equation-small"} >{props.equation}</h1>
     </div>
   )
